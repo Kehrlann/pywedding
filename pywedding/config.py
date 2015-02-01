4fd93983e1142e6f8a992e0e6ba00f0782e03ed6
Daniel Garnier <dgarnier@timsoft.com>
2015-02-01 11:52:48 +0100

    + Default config : added carousel example

--- pywedding/config.py
+++ pywedding/config.py
@@ -2,7 +2,7 @@
 GOOGLE_USERNAME     =   None                        #   ex : "john.doe@gmail.com"
 GOOGLE_PASSWORD     =   None                        #   ex : "p4$$w0rd"
 SPREADSHEET_NAME	=	'pywedding'                 #   name of the spreadsheet
-SITE_NAME           =   u'My wedding website'       #   Title of the website !
+SITE_NAME           =   u'Calvin+Suzie'             #   Title of the website !
 GIVE				=	u'Give'                     #   Label for the button on the wishlit
 ALREADY_GIVEN		=	u'Alreay given'             #   Label for items already given on the wishlit
 DETAILS				=	u'details'                  #   Label for the link on the wishlist
@@ -16,6 +16,50 @@ INFO_PAGE           =   u"""
         Default text for the information page. Here you can put pictures, links, planning details,
         contact info and whatnot. Emphasis can be achieved throught <span class="bold shadowed">bold shadowed</span>.
     </p>
+    <br>
+
+    <!-- You can also add a carousel ... -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/holder/2.4.1/holder.js"></script>
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel" style="width:660px;margin:auto;">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <img src="holder.js/660x495/sky/text:First image" alt="First">
+                <div class="carousel-caption capt">
+                    First image : caption
+                </div>
+            </div>
+            <div class="item">
+                <img src="holder.js/660x495/vine/text:Second image" alt="Second">
+                <div class="carousel-caption capt">
+                    Caption for second image
+                </div>
+            </div>
+            <div class="item">
+                <img src="holder.js/660x495/lava/text:Last image" alt="Last">
+                <div class="carousel-caption capt">
+                    Third and last caption
+                </div>
+            </div>
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
     <br>
     <p class="lead">
         Have fun editing this !
@@ -42,11 +86,11 @@ HONEYMOON_PAGE      =   u"""
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-ACCOMMODATION_LINK  =   u"Accommodation"            # the display name the Accommodation page.
+ACCOMMODATION_LINK  =   u"Housing"                  # the display name the Accommodation page.
                                                     # If None, no Accommodation page will be displayed.
 
 # Header to display before the accomodation list
-ACCOMODATION_HEADER =   u"""
+ACCOMMODATION_HEADER =  u"""
     <h1 class="cover-heading">Accommodation</h1>
 
     <p class="lead">
